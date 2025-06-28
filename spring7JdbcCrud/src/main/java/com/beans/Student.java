0c19438f00de3cc71a8536f6cdb696f68edbe137
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-28 16:26:55 +0530

    Create Student.java

---
+++ spring7JdbcCrud/src/main/java/com/beans/Student.java
@@ -0,0 +1,32 @@
+package com.beans;
+
+public class Student {
+	private int sno;
+	private String name;
+	private long mobile;
+
+	public int getSno() {
+		return sno;
+	}
+
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+
+}
