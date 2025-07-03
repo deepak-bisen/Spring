29470db58c5dc268a40f4cba16e5e49ae079d6e9
Deepak Bisen <136325518+deepak-bisen@users.noreply.github.com>
2025-07-03 17:20:09 +0530

    Update and rename SpringBootDemo5CrudApplication.java to CrudApplication.java

--- SpringBootDemo6CRUD/src/main/java/com/sp/main/CrudApplication.java
+++ SpringBootDemo6CRUD/src/main/java/com/sp/main/CrudApplication.java
@@ -11,7 +11,7 @@ import com.sp.main.dao.UserDao;
 import com.sp.main.entity.User;
 
 @SpringBootApplication
-public class SpringBootDemo5CrudApplication implements CommandLineRunner {
+public class CrudApplication implements CommandLineRunner {
 	@Autowired
 	private UserDao userDAO;
 
@@ -31,7 +31,7 @@ public class SpringBootDemo5CrudApplication implements CommandLineRunner {
 //			System.out.println("User Insertion Failed.");
 //		}
 
-//		-------------------------UPDATION------------------------------------------------------
+//		-------------------------UPDATION 1------------------------------------------------------
 //		User user = new User("Deepak", "bisen1412@gmail.com", "male", "balaghat");
 //		boolean status = userDAO.updateUser(user);
 //		if (status) {
@@ -40,6 +40,17 @@ public class SpringBootDemo5CrudApplication implements CommandLineRunner {
 //			System.out.println("User Updation Failed.");
 //		}
 
+//		-------------------------UPDATION 2------------------------------------------------------
+//		User user = userDAO.getUserByEmail("tushar@gmail.com");
+//		user.setCity("khairi");
+//		boolean status = userDAO.updateUser(user);
+//		if (status) {
+//			System.out.println("User Updated Successfully.");
+//		} else {
+//			System.out.println("User Updation Failed.");
+//		}
+
+		
 //		-------------------------DELETION------------------------------------------------------
 //		User user = new User("Deepak", "bisen1412@gmail.com", "male", "balaghat");
 //		boolean status = userDAO.deleteUserByEmail(user);
